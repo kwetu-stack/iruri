d813ccafa5c48befab3ef1ed77db323fca9b67c3
kwetu-stack <kwetudist@gmail.com>
2026-09-05 08:54:26 +0300

    Temporarily disable amenities seeding

--- app/__init__.py
+++ app/__init__.py
@@ -98,7 +98,7 @@ def create_app():
     app.register_blueprint(reports)
 
     with app.app_context():
-        seed_default_amenities()
+       # seed_default_amenities()
         seed_default_features()
         seed_default_settings()
         seed_default_roles_and_permissions()
